62eaaca625052b8983308b4e3f63c066175522c3
Devrsi0n <devrsi0n@gmail.com>
2019-05-04 09:24:43 +0800

    feat: add new domain `devrsi0n.com`

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Blog
 
-[devrsi0n.me](https://devrsion.me)
+[devrsi0n.com](https://devrsion.com)
 
 Devrsi0n's blog fork from [overreacted.io](https://github.com/gaearon/overreacted.io/tree/8266ef5e2ab69b264d85f65449e0c02e58014a38)
 
--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: 'Devrsi0n',
     author: 'Devrsi0n',
     description: '软件手艺人',
-    siteUrl: 'https://devrsi0n.me',
+    siteUrl: 'https://devrsi0n.com',
     social: {
       twitter: '@devrsi0n',
     },
@@ -66,7 +66,59 @@ module.exports = {
         trackingId: 'UA-108341680-2',
       },
     },
-    'gatsby-plugin-feed',
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                });
+              });
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/rss.xml',
+            title: "Devrsi0n's RSS Feed",
+          },
+        ],
+      },
+    },
     {
       resolve: 'gatsby-plugin-manifest',
       options: {

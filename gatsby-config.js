e28bad2b1f5017fad8c8fd0bf1ebaf1b2995afc2
devrsi0n <devrsi0n@gmail.com>
2019-05-19 22:03:51 +0800

    fix: regenerate gitalk github secret

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 const analyze = process.env.ANALYZE;
 const isEnvDev = process.env.NODE_ENV === 'development';
 
--- src/components/Comment/index.js
+++ src/components/Comment/index.js
@@ -1,36 +1,42 @@
-import React, { PureComponent } from 'react';
+import React, { useEffect, useState } from 'react';
 import pt from 'prop-types';
 import md5 from 'md5';
 import Gitalk from 'gitalk/dist/gitalk-component';
 
 import './gitalk.scss';
 
-export default class Comment extends PureComponent {
-  static propTypes = {
-    location: pt.object.isRequired,
-  };
-
-  render() {
-    const clientID = 'cc70cad9028465007126';
-    const clientSecret = '2faa230cc447c47d00cc46d9b1dc6984355ad068';
-    return (
-      <div id="gitalk-container">
-        {typeof window !== 'undefined' && Gitalk && (
-          <Gitalk
-            options={{
-              clientID,
-              clientSecret,
-              repo: 'blog',
-              owner: 'devrsi0n',
-              admin: ['devrsi0n'],
-              // Ensure uniqueness and length less than 50
-              id: md5(this.props.location.pathname),
-              // Facebook-like distraction free mode
-              distractionFreeMode: false,
-            }}
-          />
-        )}
-      </div>
-    );
-  }
+export default function Comment({ location }) {
+  const [initGitalk, setInitGitalk] = useState(false);
+  useEffect(() => {
+    // Load gitalk only when component did mount, avoid gatsby build process
+    setInitGitalk(true);
+  }, []);
+  const id = md5(location.pathname).slice(0, 5);
+  const env = process.env.NODE_ENV === 'development' ? 'DEV' : 'PROD';
+  return (
+    <div id="gitalk-container">
+      {initGitalk && typeof window !== 'undefined' && document.title && Gitalk && (
+        <Gitalk
+          options={{
+            // process.env.* cannot be destructured since they are only available at build time
+            clientID: process.env.GH_CLIENT_ID,
+            clientSecret: process.env.GH_CLIENT_SECRET,
+            repo: 'blog',
+            owner: 'devrsi0n',
+            admin: ['devrsi0n'],
+            // Ensure uniqueness and length less than 50
+            id,
+            title: document.title,
+            labels: [env],
+            // Facebook-like distraction free mode
+            distractionFreeMode: false,
+          }}
+        />
+      )}
+    </div>
+  );
 }
+
+Comment.propTypes = {
+  location: pt.object.isRequired,
+};

e7f46271086e59be5c3cdef536e730f0c12529a1
devrsi0n <devrsi0n@gmail.com>
2019-10-19 17:02:35 +0800

    fix: theme shadowing structure

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,6 @@
-exports.createPages = require('./gatsby/node/createPages');
-exports.createResolvers = require('./gatsby/node/createResolvers');
-exports.onCreateNode = require('./gatsby/node/onCreateNode');
-exports.onCreateWebpackConfig = require('./gatsby/node/onCreateWebpackConfig');
-exports.onPreBootstrap = require('./gatsby/node/onPreBootstrap');
-exports.sourceNodes = require('./gatsby/node/sourceNodes');
+exports.createPages = require('./src/gatsby/node/createPages');
+exports.createResolvers = require('./src/gatsby/node/createResolvers');
+exports.onCreateNode = require('./src/gatsby/node/onCreateNode');
+exports.onCreateWebpackConfig = require('./src/gatsby/node/onCreateWebpackConfig');
+exports.onPreBootstrap = require('./src/gatsby/node/onPreBootstrap');
+exports.sourceNodes = require('./src/gatsby/node/sourceNodes');

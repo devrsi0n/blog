23a10d50d808fc3a025f1415c6cd8029ca13ac16
Devrsi0n <devrsi0n@gmail.com>
2020-04-25 19:22:00 +0800

    test: add cypress

---
+++ cypress/integration/my-site/home.spec.js
@@ -0,0 +1,37 @@
+/// <reference types="cypress" />
+
+const config = require('../../../gatsby-config');
+
+const URL = 'http://localhost:8000/';
+
+context('Home page', () => {
+  beforeEach(() => {
+    cy.visit(URL);
+  });
+
+  it('Navigation header - Brand icon', () => {
+    cy.get('nav a img').should('exist');
+  });
+
+  it('Navigation header - Copy link', () => {
+    cy.get('nav button').should('have.length', 2);
+    const copyLink = cy.get('nav button').first();
+    const tooltip = copyLink.find('span');
+    tooltip.should('have.css', 'opacity', '0');
+    tooltip.click().should('have.css', 'opacity', '1');
+  });
+
+  it('Navigation header - Theme mode', () => {
+    // TODO: test CSS variable
+    cy.get('body').should('have.css', 'background-color', 'rgb(254, 254, 254)');
+
+    const themeButton = cy.get('nav button').last();
+    themeButton.click();
+    //rgba(17, 18, 22, 0.95);
+    cy.get('body').should('have.css', 'background-color', 'rgba(17, 18, 22, 0.95)');
+  });
+
+  it('Hero heading', () => {
+    cy.get('h1').should('contain.text', config.siteMetadata.hero.heading);
+  });
+});

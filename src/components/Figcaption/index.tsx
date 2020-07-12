6d3bca92e28f6d74823dffbc9aeba77967b429d8
Devrsi0n <devrsi0n@gmail.com>
2020-07-12 10:17:03 +0800

    refactor: rename modules

--- src/components/Figcaption/index.tsx
+++ src/components/Figcaption/index.tsx
@@ -1,1 +1,22 @@
-export { default } from './Figcaption';
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+import styled from '@emotion/styled';
+
+const Fig = styled.figcaption`
+  font-size: 14px;
+  width: 100%;
+  margin: 0 auto;
+`;
+
+type FigurationProps = React.HTMLAttributes<HTMLParagraphElement>;
+
+export default function Figcaption(props: FigurationProps) {
+  return (
+    <Fig
+      {...props}
+      sx={{
+        color: 'grey',
+      }}
+    />
+  );
+}
